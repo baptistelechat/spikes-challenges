4189c345cbbca4614d576268784d4ecb75e8cf5b
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-02-04 16:29:10 +0100

    ✨ (General) - Add Metadata for each page

--- src/app/semaine-1/page.tsx
+++ src/app/semaine-1/page.tsx
@@ -1,6 +1,12 @@
 import GoHomeButton from "@/components/GoHomeButton";
 import Modal from "@/components/semaine-1/Modal";
+import { Metadata } from "next";
 import backgroundImage from "../../assets/semaine-1/background.png";
+
+export const metadata: Metadata = {
+  title: "Challenge Web | Semaine 1",
+  description: "Challenge web of first week",
+};
 
 const Page = () => (
   <div
